2a40720f7ac6d9c7242a8d8bdab9f3322254e385
Hugo Galbarini <hgalbarini@gmail.com>
2024-06-02 22:29:14 -0400

    Task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,10 +17,16 @@ public_users.get('/', function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+    const ISBN = req.params.isbn; // Access ISBN from request parameters
+
+    // Check if the book with the provided ISBN exists in the books database
+    if (books[ISBN]) {
+        return res.status(200).json(books[ISBN]); // Return book details if found
+    } else {
+        return res.status(404).json({ message: "Book not found" }); // Book not found
+    }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
