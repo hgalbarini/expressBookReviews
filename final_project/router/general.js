9e4bca09f9c9f5fa1061ed6c021e28d6e512a526
hgalbarini <hgalbarini@gmail.com>
2024-06-08 23:58:52 -0400

    Task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -30,8 +30,21 @@ public_users.get('/isbn/:isbn', function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const booksByAuthor = [];
+
+  for (let id in books) {
+    if (books[id].author === author) {
+      booksByAuthor.push(books[id]);
+    }
+  }
+
+  if (booksByAuthor.length > 0) {
+    return res.status(200).json(booksByAuthor);
+  } else {
+    return res.status(404).json({ message: "Author not found" });
+  }
+  
 });
 
 // Get all books based on title
